d5fedaad4bdf9e876eed8a16d8367933147f3ff0
Malik Jordan <littlejamal95@gmail.com>
2024-04-13 21:16:03 -0700

    read tracer input

---
+++ setup/funcitonal_groups.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+class phytoplankton:
+    """
+    Creates class containing all phytoplankton groups considered in the model.
+    A separate subclass is created for each group.
+    Subclass contains concentrations and rates of change for each constituent.
+    """
+
+    
