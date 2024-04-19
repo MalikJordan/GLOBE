445f56555afee201bb84a5f7dff37af94d13f9f9
Malik Jordan <littlejamal95@gmail.com>
2024-04-18 19:45:47 -0700

    Changes to be committed: 	modified:   globe.py 	new file:   setup/functional_groups.py 	modified:   setup/initialize.py 	modified:   tracers.json
    Update model for class-oriented initialization.

--- setup/initialize.py
+++ setup/initialize.py
@@ -1,9 +1,15 @@
 import json
 import os
-
+from setup.functional_groups import Bacterioplankton, Inorganic, OrganicMatter, Phytoplankton, Zooplankton
 
 def initialize_model_tracers():
+    """
+    Description: Initializes BGC model. Creates a dictionary of all tracers present in model
+                 and classes for each group. 
+    """
 
+    # ----------------------------------------------------------------------------------------------------
+    num_boxes = 1
     # Initialize dictionary of tracers present in model
     tracers = {}
     count_tracers = 0
@@ -18,6 +24,7 @@ def initialize_model_tracers():
 
     if "inorganic" in tracer_info:
         inorganic = tracer_info["inorganic"]
+        inorg = Inorganic(inorganic, num_boxes)
         for key in inorganic:
             if "constituents" in inorganic:
                 constituents = inorganic[key]["constituents"]
@@ -29,12 +36,15 @@ def initialize_model_tracers():
                 count_tracers += 1            
     else:
         print("Inrganic tracers not listed in " + file_path)
+        inorg = {}
 
     if "dissolved_organic_matter" in tracer_info:
-        dom = tracer_info["dissolved_organic_matter"]
-        for key in dom:
-            if "constituents" in dom[key]:
-                constituents = dom[key]["constituents"]
+        dissolved_inorganic_matter = tracer_info["dissolved_organic_matter"]
+        dom = {}
+        for key in dissolved_inorganic_matter:
+            dom[key] = OrganicMatter(dissolved_inorganic_matter[key]["long_name"], dissolved_inorganic_matter[key]["constituents"], num_boxes)
+            if "constituents" in dissolved_inorganic_matter[key]:
+                constituents = dissolved_inorganic_matter[key]["constituents"]
                 for const in constituents:
                     tracers[count_tracers] = key + '_' + const
                     count_tracers += 1
@@ -43,12 +53,15 @@ def initialize_model_tracers():
                 count_tracers += 1
     else:
         print("Dissolved Organic Matter not listed in " + file_path)
+        dom = {}
 
     if "particulate_organic_matter" in tracer_info:
-        pom = tracer_info["particulate_organic_matter"]
-        for key in pom:
-            if "constituents" in pom[key]:
-                constituents = pom[key]["constituents"]
+        particulate_organic_matter = tracer_info["particulate_organic_matter"]
+        pom = {}
+        for key in particulate_organic_matter:
+            pom[key] = OrganicMatter(particulate_organic_matter[key]["long_name"], particulate_organic_matter[key]["constituents"], num_boxes)
+            if "constituents" in particulate_organic_matter[key]:
+                constituents = particulate_organic_matter[key]["constituents"]
                 for const in constituents:
                     tracers[count_tracers] = key + '_' + const
                     count_tracers += 1
@@ -57,13 +70,16 @@ def initialize_model_tracers():
                 count_tracers += 1
     else:
         print("Particulate Organic Matter not listed in " + file_path)
+        pom = {}
 
     # ----------------------------------------------------------------------------------------------------
     # Add living tracers (bacterioplankton, phytoplankton, zooplankton)
 
     if "bacterioplankton" in tracer_info:
         bacterioplankton = tracer_info["bacterioplankton"]
+        bac = {}
         for key in bacterioplankton:
+            bac[key] = Bacterioplankton(bacterioplankton[key]["long_name"], num_boxes)
             if "constituents" in bacterioplankton[key]:
                 constituents = bacterioplankton[key]["constituents"]
                 for const in constituents:
@@ -74,10 +90,13 @@ def initialize_model_tracers():
                 count_tracers += 1
     else:
         print("Bacterioplankton not listed in " + file_path)
+        bac = {}
 
     if "phytoplankton" in tracer_info:
         phytoplankton = tracer_info["phytoplankton"]
+        phyto = {}
         for key in phytoplankton:
+            phyto[key] = Phytoplankton(phytoplankton[key]["long_name"], phytoplankton[key]["constituents"], num_boxes)
             if "constituents" in phytoplankton[key]:
                 constituents = phytoplankton[key]["constituents"]
                 for const in constituents:
@@ -88,10 +107,13 @@ def initialize_model_tracers():
                 count_tracers += 1
     else:
         print("Phytoplankton not listed in " + file_path)
+        phyto = {}
 
     if "zooplankton" in tracer_info:
         zooplankton = tracer_info["zooplankton"]
+        zoo = {}
         for key in zooplankton:
+            zoo[key] = Zooplankton(zooplankton[key]["long_name"], num_boxes)
             if "constituents" in zooplankton[key]:
                 constituents = zooplankton[key]["constituents"]
                 for const in constituents:
@@ -102,5 +124,6 @@ def initialize_model_tracers():
                 count_tracers += 1
     else:
         print("Zooplankton not listed in " + file_path)
+        zoo = {}
 
-    return tracers
+    return tracers, bac, dom, inorg, phyto, pom, zoo
